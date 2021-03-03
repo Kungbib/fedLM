da622ce021b2bc6502588e1a6121993ddd884974
mattiasakesson <ma10s@hotmail.com>
2021-03-03 16:10:37 +0100

    add client_settings

--- FEDn-client-KB/client/train.py
+++ FEDn-client-KB/client/train.py
@@ -8,7 +8,7 @@ import configure_pretraining
 from fedn.utils.pytorchhelper import PytorchHelper
 from scripts.clienthelper_tfestimator import load_weights_to_model, get_weights_from_model, get_global_step
 
-def train(data_dir, model_name, settings):
+def train(data_dir, model_name, settings, client_settings):
     print("-- RUNNING TRAINING --", flush=True)
     import os
     arr = os.listdir(data_dir)
@@ -16,10 +16,21 @@ def train(data_dir, model_name, settings):
 
     global_step = get_global_step(data_dir, model_name)
 
+    try:
+        train_batch_size = client_settings['train_batch_size']
+    except:
+        train_batch_size = settings['train_batch_size']
+
+    try:
+        num_train_steps = client_settings['num_train_steps']
+    except:
+        num_train_steps = settings['num_train_steps']
+
+    print("global_step: ", global_step)
     hparams = {
-        "num_train_steps": global_step + settings['num_train_steps'],
+        "num_train_steps": global_step + num_train_steps,
         "save_checkpoints_steps": 10,
-        "train_batch_size": settings['train_batch_size']
+        "train_batch_size": train_batch_size
     }
 
     tf.logging.set_verbosity(tf.logging.ERROR)
@@ -35,6 +46,14 @@ if __name__ == '__main__':
             settings = dict(yaml.safe_load(fh))
         except yaml.YAMLError as e:
             raise(e)
+
+    client_settings = {}
+    with open('/app/client_settings.yaml', 'r') as fh:
+        try:
+            client_settings = dict(yaml.safe_load(fh))
+            print("client_settings: ", client_settings)
+        except yaml.YAMLError as e:
+            raise(e)
     # TODO: define model_name and data_dir in settings
     model_name = 'electra_small_owt'
     data_dir = '/app/data'
@@ -43,7 +62,7 @@ if __name__ == '__main__':
     weights = helper.load_model(sys.argv[1])
 
     load_weights_to_model(weights, data_dir, model_name)
-    train(data_dir, model_name, settings)
+    train(data_dir, model_name, settings, client_settings)
     weights = get_weights_from_model(data_dir, model_name)
     ret = helper.save_model(weights, sys.argv[2])
     print("saved as: ", ret)
--- FEDn-client-KB/client/validate.py
+++ FEDn-client-KB/client/validate.py
@@ -9,10 +9,12 @@ import configure_pretraining
 from fedn.utils.pytorchhelper import PytorchHelper
 from scripts.clienthelper_tfestimator import load_weights_to_model, get_weights_from_model
 
-def validate(data_dir, model_name):
+def validate(data_dir, model_name, client_settings):
     print("-- RUNNING VALIDATE --", flush=True)
 
-    hparams = {"num_eval_steps": 1}#, "train_batch_size": 128}
+
+
+    hparams = {"num_eval_steps": client_settings['num_eval_steps'], "eval_batch_size": client_settings['eval_batch_size']}
 
     conf = configure_pretraining.PretrainingConfig(
         model_name, data_dir, **hparams)
@@ -31,20 +33,20 @@ def validate(data_dir, model_name):
 
 if __name__ == '__main__':
 
-    with open('settings.yaml', 'r') as fh:
+    with open('/app/client_settings.yaml', 'r') as fh:
         try:
-            settings = dict(yaml.safe_load(fh))
+            client_settings = dict(yaml.safe_load(fh))
         except yaml.YAMLError as e:
             raise(e)
     # TODO: define model_name and data_dir in settings
     model_name = 'electra_small_owt'
-    data_dir = '../data'
+    data_dir = '/app/data'
 
     helper = PytorchHelper()
     weights = helper.load_model(sys.argv[1])
 
     load_weights_to_model(weights, data_dir, model_name)
-    report = validate(data_dir, model_name)
+    report = validate(data_dir, model_name, client_settings)
     print("report: ", report)
     print("sys.argv[2]: ", sys.argv[2])
     with open(sys.argv[2], "w") as fh:
