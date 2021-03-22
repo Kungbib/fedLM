a4f691d2a1c1588e1b75d139e9b759f1730a04c2
Robin Kurtz <robin.kurtz@kb.se>
2021-03-22 16:55:51 +0100

    plot control status

--- logs/processfedndata.py
+++ logs/processfedndata.py
@@ -1,19 +1,27 @@
 import json
-import numpy as np
-def get_training_results(path, metric = 'test_accuracy'):
-    with open(path) as f:
-      json_data = json.load(f)
-    model_validation = [x for x in json_data if x['type'] == 'MODEL_VALIDATION']
-    validations = {}
-    for post in model_validation:
-        e = json.loads(post['data'])
-        try:
-            validations[e['modelId']].append(float(json.loads(e['data'])[metric]))
-        except KeyError:
-            validations[e['modelId']] = [float(json.loads(e['data'])[metric])]
-    model_ids = []
-    metric_results = []
-    for model_id, acc in validations.items():
-        model_ids.append(model_id)
-        metric_results.append(np.mean(acc))
-    return model_ids, metric_results
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+
+def read_status(fname):
+    with open(fname) as fh:
+        json_data = json.load(fh)
+        results = []
+        for entry in filter(lambda x: x["type"] == "MODEL_VALIDATION", json_data):
+            entry = json.loads(entry["data"])
+            results.append((json.loads(entry["data"]), entry["modelId"]))
+    return results
+
+
+def plot_status(results, metric="loss"):
+    xs = defaultdict(list)
+    for x, name in results:
+        xs[name].append(x[metric])
+    plt.plot(list(xs.values()), "-")
+    plt.show()
+
+
+if __name__ == "__main__":
+    import sys
+    data = read_status(sys.argv[1])
+    plot_status(data, "loss")
